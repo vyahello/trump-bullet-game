985414cdc3d021ee86a6c9c1f2119bc3ef6c817a
Volodymyr Yahello <vyahello@gmail.com>
2019-01-28 00:10:18 +0200

    Add visualisations

---
+++ lib/visual.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+from pygame import display, Surface
+
+
+class Window(ABC):
+    """The class represents abstract window."""
+
+    @abstractmethod
+    def parent(self) -> Surface:
+        """Returns parent window."""
+        pass
+
+    @abstractmethod
+    def fill(self, color: Tuple[int, ...]) -> None:
+        """Fills window with color."""
+        pass
+
+
+class Display(ABC):
+    """The class represents display abstraction."""
+
+    @abstractmethod
+    def set_resolution(self) -> Window:
+        """Sets resolution for a game."""
+        pass
+
+    @abstractmethod
+    def set_title(self) -> None:
+        """Sets title for a game."""
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        """Updates game display."""
+        pass
+
+
+class GameDisplay(Display):
+    """The class represents game display."""
+
+    def __init__(self, resolution: Tuple[int, int], title: str) -> None:
+        self._res = resolution
+        self._title = title
+
+    def set_resolution(self) -> Window:
+        """Sets resolution for a game."""
+        return GameWindow(display.set_mode(self._res))
+
+    def set_title(self) -> None:
+        """Sets title for a game."""
+        display.set_caption(self._title)
+
+    def update(self) -> None:
+        """Updates game display."""
+        display.update()
+
+
+class GameWindow(Window):
+    """The class represents game window."""
+
+    def __init__(self, window: Surface) -> None:
+        self._win = window
+
+    @property
+    def parent(self) -> Surface:
+        return self._win
+
+    def fill(self, color: Tuple[int, ...]) -> None:
+        self._win.fill(color)
